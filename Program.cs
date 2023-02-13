c17d3f17a4f554ea5a67b96a1e9a5e103576abec
Judisk <122523925+Judisk@users.noreply.github.com>
2023-02-13 13:28:31 +0300

    fixes task 34

--- Program.cs
+++ Program.cs
@@ -6,22 +6,28 @@ void TaskNum(int number)
     System.Console.WriteLine($"__Задание {number}___");
 }
 
+void showArraymetod (int [] testArray)
+{
+    int [ ] showArray = testArray;
+    var str = string.Join(",", showArray);
+    Console.WriteLine(str);
+}
 
 int[] createArray(double degree, int maxSizeArray)
 {
     var rnd = new Random();
     if (degree!=0 && maxSizeArray!=0)
     {
-    double maxDegreeDouble = Math.Pow(10,degree);
-    int maxDegree = Convert.ToInt32(maxDegreeDouble);
-    int minDegree = maxDegree/10;
-    int sizeArray = rnd.Next(1,maxSizeArray);
-    int [] rndArray = new int [sizeArray];
-    for (int i = 0; i<sizeArray; i++)
-    {
-        rndArray[i] = rnd.Next(minDegree,maxDegree); 
-    }
-    return rndArray;
+        double maxDegreeDouble = Math.Pow(10,degree);
+        int maxDegree = Convert.ToInt32(maxDegreeDouble);
+        int minDegree = maxDegree/10;
+        int sizeArray = rnd.Next(1,maxSizeArray);
+        int [] rndArray = new int [sizeArray];
+        for (int i = 0; i<sizeArray; i++)
+        {
+            rndArray[i] = rnd.Next(minDegree,maxDegree); 
+        }
+        return rndArray;
     }
     else
     {
@@ -37,20 +43,13 @@ int[] createArray(double degree, int maxSizeArray)
 int resArray(int[] test1 )
 {   
     showArraymetod(test1);
-    int checkRes=0;
     int count = 0 ;
     for (int i = 0 ; i <test1.Length;i++)
     {
-        while (test1[i]>1)
-        {
-            checkRes = test1[i]%10;
-            test1[i]/=10;
-            if (checkRes%2==0)
+            if (test1[i]%2==0)
             {
                 ++count;
             }
-            checkRes=0;
-        }
     }
     return count;
 }
@@ -60,8 +59,6 @@ int resArray(int[] test1 )
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
-
-
 
 int nechNum(int [] testArray)
 {
@@ -76,12 +73,7 @@ int nechNum(int [] testArray)
     }
     return Res;
 }
-void showArraymetod (int [] testArray)
-{
-    int [ ] showArray = testArray;
-    var str = string.Join(",", showArray);
-    Console.WriteLine(str);
-}
+
 TaskNum(34);
 System.Console.WriteLine(resArray(createArray(3,10)));
 
